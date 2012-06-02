560781d05c142d6da34d16bf7007fc1148462f77
adambouchard <adambouchard@gmail.com>
2012-06-02 11:11:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PopUpBurlington::Application.config.secret_token = '94e6a7bccd9d8cd35e81602cce05dcb9e0ca0d9998893bc319bb975976920a354d047d6b684d255da96a270f8453b0b4de9f330d4ec332de4f8685dd4a2d29b2'
